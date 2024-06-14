a999adc0a5184853710c540dd124a374741259d1
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-06-14 09:54:38 +0200

    push new service

--- Python/app/service/main.py
+++ Python/app/service/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
-
+import shutil
+import os
+from pydantic import BaseModel
+import json
 
 app = FastAPI()
+
+UPLOAD_FOLDER = 'samples'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+# Définir le modèle de données
+class Item(BaseModel):
+    data: dict
 
 # start the server with the command: uvicorn main:app --reload
 
@@ -11,9 +22,15 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/upload-audio/")
-async def upload_audio(file: UploadFile = File(...)):
-    file_location = f"~/samples/{file.filename}"
-    with open(file_location, "wb") as f:
-        f.write(file.file.read())
-    
-    return JSONResponse(content={"filename": file.filename, "location": file_location})
+async def upload_json(file: UploadFile = File(...)):
+    file_location = os.path.join(UPLOAD_FOLDER, file.filename)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"info": f"file '{file.filename}' saved at '{file_location}'"}
+
+@app.post("/upload-json/")
+async def upload_json(item: Item):
+    file_location = os.path.join(UPLOAD_FOLDER, 'uploaded_content.json')
+    with open(file_location, "w") as buffer:
+        json.dump(item.data, buffer)
+    return {"info": f"content saved at '{file_location}'"}
